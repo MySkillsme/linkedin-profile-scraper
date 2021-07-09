9fba3334d402e59d07a83e6dabd1a9e4bd14ec9c
Ananthu <ananthulb@outlook.com>
2021-07-09 17:32:34 +1200

    local

--- dist/index.js
+++ dist/index.js
@@ -381,7 +381,8 @@ class LinkedInProfileScraper {
 
                         // featured section start
                         const featuredSection = document.querySelector('section.pab-featured-section')
-                        let featuredLinks = [];
+                        let featuredLinksGithub = [];
+                        let featuredLinksGitlab = [];
                         if (featuredSection != null) {
 
                             const featuredItems = featuredSection.querySelectorAll('li.artdeco-carousel__item')
@@ -390,10 +391,10 @@ class LinkedInProfileScraper {
                                 const itemLink = itemLinkElement.getAttribute("href");
 
                                 if (itemLink.includes("github.com")) {
-                                    featuredLinks.push(itemLink);
+                                    featuredLinksGithub.push(itemLink);
                                 }
                                 else if (itemLink.includes("gitlab.com")) {
-                                    featuredLinks.push(itemLink);
+                                    featuredLinksGitlab.push(itemLink);
                                 }
                             })
                         }
@@ -454,7 +455,8 @@ class LinkedInProfileScraper {
                             photo,
                             description,
                             url,
-                            featuredLinks
+                            featuredLinksGithub,
+                            featuredLinksGitlab
                         };
                     });
                     const userProfile = Object.assign(Object.assign({}, rawUserProfileData), {
